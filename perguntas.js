c0acb0bc76a1a0f8f363a46e2e252d608f6a7923
aleeska <aleska.tomparovski@escola.pr.gov.br>
2025-07-01 17:09:37 -0300

    perguntas

---
+++ perguntas.js
@@ -0,0 +1,16 @@
+function criaCartao( categoria, pergunta,resposta){
+    'CIÊNCIAS'
+    'Como é feito a Fotossíntese?'
+    'Pelo sol'
+    console.log(categoria, pergunta, resposta);
+}
+criaCartao(
+    'PROGRAMAÇÃO'
+    'O que é javaScript?'
+    'É uma linguagem de programação!'
+)
+criaCartao(
+    'GEOGRAFIA'
+    'Qual a capital do Brasil'
+    'Brasília'
+)
