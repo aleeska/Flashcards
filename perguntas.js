cb13db3bb2ca798e54f0e3f727c55a1ed7548cb0
aleeska <aleska.tomparovski@escola.pr.gov.br>
2025-07-01 17:10:40 -0300

    perguntas e respostas

--- perguntas.js
+++ perguntas.js
@@ -2,7 +2,6 @@ function criaCartao( categoria, pergunta,resposta){
     'CIÊNCIAS'
     'Como é feito a Fotossíntese?'
     'Pelo sol'
-    console.log(categoria, pergunta, resposta);
 }
 criaCartao(
     'PROGRAMAÇÃO'
